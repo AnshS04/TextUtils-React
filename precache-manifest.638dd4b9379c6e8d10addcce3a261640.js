1fe7dd9599732e45c9665f577e8392108958565b
AnshSachdeva23 <sachdevaansh2303@gmail.com>
2023-07-24 14:20:11 +0530

    Updates

---
+++ precache-manifest.638dd4b9379c6e8d10addcce3a261640.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a15f31b926a540771428",
+    "url": "/TextUtils-React/static/css/main.e2578cd9.chunk.css"
+  },
+  {
+    "revision": "a15f31b926a540771428",
+    "url": "/TextUtils-React/static/js/main.fc72dc0b.chunk.js"
+  },
+  {
+    "revision": "a4f9459a308288b6214f",
+    "url": "/TextUtils-React/static/js/runtime~main.02905c84.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/TextUtils-React/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/TextUtils-React/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "673065c1ea9c95c3c1faa6b71489dd7e",
+    "url": "/TextUtils-React/index.html"
+  }
+];
